10a08b756435773d04268041ae30942e55a10edd
Alireza <42074597+arbor0711@users.noreply.github.com>
2024-03-03 15:13:46 +0100

    Add some style

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,8 +8,8 @@ interface Props {
 
 const Layout: React.FC<Props> = ({ pageTitle, children }) => (
   <>
-    <nav>
-      <ul>
+    {/* <nav className="bg-gray-800 py-4">
+      <ul className="row">
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -17,6 +17,34 @@ const Layout: React.FC<Props> = ({ pageTitle, children }) => (
           <Link to="/about">About</Link>
         </li>
       </ul>
+    </nav> */}
+    <nav className="bg-gray-800 py-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <a href="/" className="text-white text-xl font-bold">
+              aX
+            </a>
+          </div>
+          <div className="hidden md:block">
+            <a href="/" className="text-gray-300 hover:text-white px-3 py-2">
+              Home
+            </a>
+            <a
+              href="/about"
+              className="text-gray-300 hover:text-white px-3 py-2"
+            >
+              About
+            </a>
+            <a
+              href="/blogs"
+              className="text-gray-300 hover:text-white px-3 py-2"
+            >
+              Blog
+            </a>
+          </div>
+        </div>
+      </div>
     </nav>
     <main>
       <h1>{pageTitle}</h1>
