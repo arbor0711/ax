639dc1136df62e281d92c3829daab86ce5505e81
Alireza <42074597+arbor0711@users.noreply.github.com>
2024-03-03 22:57:22 +0100

    Use graphQL to pull data

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,4 @@
-import { HeadFC, Link } from "gatsby";
+import { graphql, useStaticQuery } from "gatsby";
 import * as React from "react";
 import NavBar from "./navbar";
 
@@ -7,13 +7,25 @@ interface Props {
   children?: React.ReactNode;
 }
 
-const Layout: React.FC<Props> = ({ pageTitle, children }) => (
-  <>
-    <NavBar />
-    <main className="p-5">
-      <h1 className="">{pageTitle}</h1>
-      {children}
-    </main>
-  </>
-);
+const Layout: React.FC<Props> = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  return (
+    <>
+      <header>{data.site.siteMetadata.title}</header>
+      <NavBar />
+      <main className="p-5">
+        <h1 className="">{pageTitle}</h1>
+        {children}
+      </main>
+    </>
+  );
+};
 export default Layout;
