d6246be64ca55a1b4927a95a5525dd27d4e68cba
Alireza <42074597+arbor0711@users.noreply.github.com>
2024-04-18 23:32:33 +0200

    Fix security issues: add rel="noreferrer" to anchor tag Using target="_blank"

--- src/components/portfolio/project.jsx
+++ src/components/portfolio/project.jsx
@@ -21,16 +21,17 @@ const TestPage = ({ data }) => {
   } = data.markdownRemark.frontmatter;
   return (
     <div className="w-screen min-h-screen p-5 sm:p-10 md:p-16 text-texty dark:text-gray-100 ">
-      {/* <div className="absolute top-0 left-0 right-0  w-screen h-screen">
-        <img src={imgSrc.childImageSharp.fluid.src} alt="some text" />
-      </div> */}
-
       <h2 className="text-h3 md:text-h2 px-10 text-center mb-10 md:mb-20 ">
         {title}
       </h2>
       <div className="flex flex-col md:flex-row  px-10  justify-around items-center bg-transparent">
         <aside className="flex flex-col items-center w-2/5">
-          <a className=" flex mb-5 z-50" href={address} target="_blank">
+          <a
+            className=" flex mb-5 z-50"
+            href={address}
+            rel="noreferrer"
+            target="_blank"
+          >
             Site
             <span className="-rotate-45 ml-1">
               <FaArrowRight size={15} />
@@ -60,31 +61,16 @@ const TestPage = ({ data }) => {
             <div className=" font-light text-sm border-b-2 border-gray-100 dark:border-gray-900 mb-1 md:mb-2">
               <span className=" italic font-medium">Service Type:</span>
               {service}
-              {/* {data.service.map((item, id) => (
-                  <span className="  ml-5" key={id}>
-                    {item}
-                  </span>
-                ))} */}
             </div>
 
             <div className=" font-light text-sm border-b-2 border-gray-100 dark:border-gray-900 mb-1 md:mb-2">
               <span className=" italic font-medium">Current Condition:</span>
-              <span className="  ml-5">
-                {condition}
-                {/* {data.condition
-                    ? "Website Up and Running"
-                    : "Website Currently Unavailable"} */}
-              </span>
+              <span className="  ml-5">{condition}</span>
             </div>
 
             <div className=" font-light text-sm border-b-2 border-gray-100 dark:border-gray-900 mb-1 md:mb-2">
               <span className=" italic font-medium ">My Role:</span>
               {role}
-              {/* {data.role.map((item, id) => (
-                  <span className="  ml-5" key={id}>
-                    {item}
-                  </span>
-                ))} */}
             </div>
 
             {person && (
